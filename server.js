3a25be257c1933dba772ae2f84f23e79d657377b
souza-eduardo <esouzasantos085@gmail.com>
2024-01-26 10:24:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => console.log(error));
+
+database.once('connected', () => console.log('Database connected'));
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Listening at http:/localhost:${PORT}`));
