1295d14af4a76aaf8f88530c4b28a88bb73789a5
souza-eduardo <esouzasantos085@gmail.com>
2024-01-27 10:51:13 -0400

    criei o html (frontend)

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
-
+const cors = require('cors');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 
+const app = express();
 const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
 
 const database = mongoose.connection;
 
-database.on('error', (error) => console.log(error));
+database.on('error', (error) => console.log('Error connecting to database:', error));
 
 database.once('connected', () => console.log('Database connected'));
 
 app.use(express.json());
-
-const routes = require('./routes/routes');
-
+app.use(cors());
 app.use('/', routes);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening at http:/localhost:${PORT}`));
